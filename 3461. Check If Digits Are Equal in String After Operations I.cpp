d467d89e67b23199327290657595656616d22468
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-11-26 14:00:53 +0530

    Add Solution for 3461 leetcode
    Implement a solution to check if digits in a string are equal after operations.

---
+++ 3461. Check If Digits Are Equal in String After Operations I.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        for(int i = 0; i < s.length() - 2; i++){
+            for(int j = 0; j < s.length() - 1; j++){
+                s[j] = ((s[j] - '0') + (s[j + 1] - '0')) % 10 + '0';
+            }
+        }
+        return s[0] == s[1];
+    }
+};
