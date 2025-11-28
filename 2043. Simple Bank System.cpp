62ca59de44fa0885ba022ae81709ce8191ff45bd
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-11-28 23:03:35 +0530

    Implement 2043 Simple Bank System class

---
+++ 2043. Simple Bank System.cpp
@@ -0,0 +1,44 @@
+class Bank {
+public:
+    vector<long long> balance;
+    Bank(vector<long long>& balance) {
+        this->balance = balance;
+    }
+    
+    bool check(int acc){
+        return acc >= 1 && acc <= balance.size();
+    }
+
+    bool transfer(int account1, int account2, long long money) {
+        if(check(account1) && check(account2) && balance[account1 - 1] >= money){
+            balance[account1 - 1] -= money;
+            balance[account2 - 1] += money;
+            return true;
+        }
+        return false;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(check(account)){
+            balance[account - 1] += money;
+            return true;
+        }
+        return false;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(check(account) && balance[account - 1] >= money){
+            balance[account - 1] -= money;
+            return true;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
