ad58e1b8182b1210839a80c7dff85bc13ab856f3
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-12-04 22:38:26 +0530

    Implement 3234 function to count dominant one substrings

---
+++ 3234. Count the Number of Substrings With Dominant Ones.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        vector<int> pre(s.size() + 1);
+        pre[0] = -1;
+        for (int i = 0; i < s.size(); i++) {
+            if (i == 0 || (i > 0 && s[i - 1] == '0')) {
+                pre[i + 1] = i;
+            } else {
+                pre[i + 1] = pre[i];
+            }
+        }
+        int res = 0;
+        for (int i = 1; i <= s.size(); i++) {
+            int cnt0 = s[i - 1] == '0';
+            int j = i;
+            while (j > 0 && cnt0 * cnt0 <= s.size()) {
+                int cnt1 = (i - pre[j]) - cnt0;
+                if (cnt0 * cnt0 <= cnt1) {
+                    res += min(j - pre[j], cnt1 - cnt0 * cnt0 + 1);
+                }
+                j = pre[j];
+                cnt0++;
+            }
+        }
+        return res;
+    }
+};
