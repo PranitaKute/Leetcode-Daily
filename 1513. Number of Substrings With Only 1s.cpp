06cfc694e9420605d73abe64051081f1d2b032eb
Pranita Kute <97874560+PranitaKute@users.noreply.github.com>
2025-12-07 12:29:48 +0530

    Implement solution for 1513 counting substrings of 1s

---
+++ 1513. Number of Substrings With Only 1s.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSub(string s) {
+        int MOD = 1000000007;
+        long long cur = 0, ans = 0;
+        for(char c : s){
+            if(c == '1'){
+                cur = (cur + 1) % MOD;
+            }
+            else{
+                cur = 0;
+            }
+            ans = (ans + cur) % MOD;
+        }
+        return ans;
+    }
+};
